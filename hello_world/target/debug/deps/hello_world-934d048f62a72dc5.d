84a6d03aed842c1efaf9f444c0e0baa4972fa55d
Olawumi Olasunkanmi <wumirose@gmail.com>
2025-02-13 21:45:21 +0000

    added the first rust code

---
+++ hello_world/target/debug/deps/hello_world-934d048f62a72dc5.d
@@ -0,0 +1,5 @@
+/workspaces/rust-tutorial/hello_world/target/debug/deps/hello_world-934d048f62a72dc5: src/main.rs
+
+/workspaces/rust-tutorial/hello_world/target/debug/deps/hello_world-934d048f62a72dc5.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/workspaces/rust-tutorial/hello_world/target/debug/hello_world: /workspaces/rust-tutorial/hello_world/src/main.rs
